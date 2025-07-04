cc870e25b1ebac76ac352d23ae9b4fb2b4efdc1e
Nilton Silva <165297557+j-nilton@users.noreply.github.com>
2025-07-04 15:30:48 -0300

    Atualização do html e css dos arquivos layout.tsx e page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Image from 'next/image'
 
 export default function RootLayout({
   children,
@@ -8,7 +9,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        {children}
+        <header className="header">
+          <div className="left-section">
+            <div className="menu-icon">
+              {/* Ícone do menu hambúrguer */}
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2854C5"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+            </div>
+
+            <div className="logo-container">
+              <Image src="/images/logoUpTrade.png" alt="Logo UpTrade" width={40}
+                height={40} />
+              <span className="logo-text">UpTrade</span>
+            </div>
+          </div>
+
+          <div className="right-section">
+            <div className="search-bar">
+              <input type="text" placeholder="Pesquisar..." />
+              <button>
+                {/* Ícone de lupa */}
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2854C5"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg>
+              </button>
+            </div>
+
+            <div className="user-icon">
+              {/* Ícone de usuário */}
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2854C5"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" /></svg>
+            </div>
+          </div>
+        </header>
+
+        <main>{children}</main>
+
+        <footer className="footer">
+          <p>© {new Date().getFullYear()} UpTrade. Todos os direitos reservados.</p>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
-import "./globals.css"
+import "./globals.css";
 
-export default function PaginaInicial(){
+export default function PaginaInicial() {
   return (
-    <div id="escolhaUmId">
-      <h1>Página Inicial</h1>
+    <div className="pagina-inicial">
+      <div className="painel">
+        <button className="btn btn-carteiras">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24">
+            <path d="M21 7H3V5a2 2 0 012-2h14a2 2 0 012 2v2zM3 9h18v10a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+          </svg>
+          <span>Carteiras</span>
+        </button>
+
+        <button className="btn btn-metas">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24">
+            <path d="M12 2a10 10 0 1010 10A10 10 0 0012 2zm0 2a8 8 0 110 16A8 8 0 0112 4zm0 2a6 6 0 00-1 11.91V13h-2v-2h2V9a3 3 0 013-3h1v2h-1a1 1 0 00-1 1v2h2v2h-2v2.91A6 6 0 0012 6z" />
+          </svg>
+          <span>Metas</span>
+        </button>
+
+        <button className="btn btn-caixinhas">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24">
+            <path d="M3 4a2 2 0 012-2h14a2 2 0 012 2v16l-6-3-6 3V4z" />
+          </svg>
+          <span>Caixinhas de<br />Poupança</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 /*
