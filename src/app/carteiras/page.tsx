cc3495e1874deee6e3e85f6a039c2717084f6e64
Nilton Silva <165297557+j-nilton@users.noreply.github.com>
2025-07-04 23:23:33 -0300

    Atualização do html e css responsável pela tela "carteiras".

--- src/app/carteiras/page.tsx
+++ src/app/carteiras/page.tsx
@@ -1,12 +1,46 @@
 import "./carteiras.css"
 
-export default function Carteiras(){
+export default function Carteiras() {
   return (
-    <div id="escolhaUmId">
-      <h1><b>Área de Carteiras</b></h1>
+    <div className="wallet-app-wallets-container">
+      {/* Cabeçalho da seção */}
+      <div className="wallet-app-wallets-header">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M336-120q-91 0-153.5-62.5T120-336q0-38 13-74t37-65l142-171-97-194h530l-97 194 142 171q24 29 37 65t13 74q0 91-63 153.5T624-120H336Zm144-200q-33 0-56.5-23.5T400-400q0-33 23.5-56.5T480-480q33 0 56.5 23.5T560-400q0 33-23.5 56.5T480-320Zm-95-360h190l40-80H345l40 80Zm-49 480h288q57 0 96.5-39.5T760-336q0-24-8.5-46.5T728-423L581-600H380L232-424q-15 18-23.5 41t-8.5 47q0 57 39.5 96.5T336-200Z"/></svg>
+        <span>Carteiras</span>
+      </div>
+
+      {/* Botões de ação */}
+      <div className="wallet-app-wallets-actions">
+        <button className="wallet-app-sort-button">Ordenar</button>
+        <button className="wallet-app-create-button">Criar nova carteira</button>
+      </div>
+
+      {/* Cartões de carteira */}
+      <div className="wallet-app-wallet-card">
+        <span className="wallet-app-wallet-name">Nova carteira</span>
+        <div className="wallet-app-wallet-details">
+          <span className="wallet-app-wallet-balance wallet-app-positive-balance">Saldo: R$ 177,84</span>
+          <button className="wallet-app-delete-wallet" title="Excluir">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+          </button>
+        </div>
+      </div>
+
+      <div className="wallet-app-wallet-card">
+        <span className="wallet-app-wallet-name"></span>
+        <div className="wallet-app-wallet-details">
+          <span className="wallet-app-wallet-balance wallet-app-negative-balance">Saldo: R$ -24,63</span>
+        </div>
+      </div>
+
+      <div className="wallet-app-wallet-card">
+        <span className="wallet-app-wallet-name"></span>
+        <div className="wallet-app-wallet-details"></div>
+      </div>
     </div>
-  )
+  );
 }
+
 
 /*
 Esse arquivo deverá ser usado para estrutura as páginas do sistema a que eles se referem, de acordo 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,20 @@ import "./globals.css";
 
 export default function PaginaInicial() {
   return (
-    <div className="pagina-inicial">
-      <div className="painel">
-        <button className="btn btn-carteiras">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24">
-            <path d="M21 7H3V5a2 2 0 012-2h14a2 2 0 012 2v2zM3 9h18v10a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-          </svg>
+    <div className="wallet-app-homepage">
+      <div className="wallet-app-panel">
+        <button className="wallet-app-btn wallet-app-btn-wallets">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M336-120q-91 0-153.5-62.5T120-336q0-38 13-74t37-65l142-171-97-194h530l-97 194 142 171q24 29 37 65t13 74q0 91-63 153.5T624-120H336Zm144-200q-33 0-56.5-23.5T400-400q0-33 23.5-56.5T480-480q33 0 56.5 23.5T560-400q0 33-23.5 56.5T480-320Zm-95-360h190l40-80H345l40 80Zm-49 480h288q57 0 96.5-39.5T760-336q0-24-8.5-46.5T728-423L581-600H380L232-424q-15 18-23.5 41t-8.5 47q0 57 39.5 96.5T336-200Z"/></svg>
           <span>Carteiras</span>
         </button>
 
-        <button className="btn btn-metas">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24">
-            <path d="M12 2a10 10 0 1010 10A10 10 0 0012 2zm0 2a8 8 0 110 16A8 8 0 0112 4zm0 2a6 6 0 00-1 11.91V13h-2v-2h2V9a3 3 0 013-3h1v2h-1a1 1 0 00-1 1v2h2v2h-2v2.91A6 6 0 0012 6z" />
-          </svg>
+        <button className="wallet-app-btn wallet-app-btn-goals">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-80q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm0-80q66 0 113-47t47-113q0-66-47-113t-113-47q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-80q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Z"/></svg>
           <span>Metas</span>
         </button>
 
-        <button className="btn btn-caixinhas">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24">
-            <path d="M3 4a2 2 0 012-2h14a2 2 0 012 2v16l-6-3-6 3V4z" />
-          </svg>
+        <button className="wallet-app-btn wallet-app-btn-savings">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-640v440h560v-440H640v320l-160-80-160 80v-320H200Zm0 520q-33 0-56.5-23.5T120-200v-499q0-14 4.5-27t13.5-24l50-61q11-14 27.5-21.5T250-840h460q18 0 34.5 7.5T772-811l50 61q9 11 13.5 24t4.5 27v499q0 33-23.5 56.5T760-120H200Zm16-600h528l-34-40H250l-34 40Zm184 80v190l80-40 80 40v-190H400Zm-200 0h560-560Z"/></svg>
           <span>Caixinhas de<br />Poupança</span>
         </button>
       </div>
